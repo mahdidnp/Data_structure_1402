7b62b735b9c5b719f7b0d57af99722d1c3313fb3
mahdidnp <mahdidinpazhouh@gmail.com>
2023-11-06 13:48:16 +0330

    recursive hanoi tower code

--- hanoi.py
+++ hanoi.py
@@ -1,10 +1,11 @@
-def TowerOfHanoi(n, from_rod, to_rod, aux_rod): 
-	if n == 0: 
+def TowerOfHanoi(n , source, auxiliary, destination):
+	if n==1:
+		print ("Move disk 1 from source",source,"to destination",destination)
 		return
-	TowerOfHanoi(n-1, from_rod, aux_rod, to_rod) 
-	print("Move disk", n, "from rod", from_rod, "to rod", to_rod) 
-	TowerOfHanoi(n-1, aux_rod, to_rod, from_rod) 
+	TowerOfHanoi(n-1, source, destination, auxiliary)
+	print ("Move disk",n,"from source",source,"to destination",destination)
+	TowerOfHanoi(n-1, auxiliary, destination, source)
+		
 
-N = 3
-
-TowerOfHanoi(N, 'A', 'C', 'B') 
+n = 3
+TowerOfHanoi(n,'A','B','C') 

3853d9e66b2daa0de05a74cfb0ed874b2d7636f6
zhrad81 <zhradashti8196@gmail.com>
2023-11-22 12:07:15 +0330

    pop is empty added

--- stack/stack.py
+++ stack/stack.py
@@ -2,3 +2,13 @@ class stack():
 
     def __init__(self):
         self.stack=[]
+    def pop(self):
+        if not self.is_empty():   
+            item=self.stack[-1]
+            del self.stack[-1]
+            return item
+    def is_empty(self):
+        if len(self.stack) == 0:
+            return True
+        else:
+            return False

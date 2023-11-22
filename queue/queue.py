71413929b27cba9d4ddc69e148989cf3ee5474cc
zhrad81 <zhradashti8196@gmail.com>
2023-11-22 14:13:21 +0330

    new methods added

--- queue/queue.py
+++ queue/queue.py
@@ -5,3 +5,16 @@ class Queue:
         self.size=size
         self.queue = [None]*size
 
+    def dequeue(self):
+        if self.rear==self.front:
+            self.is_empty()
+        for i in range(self.size - 1):
+            self.queue[i] = self.queue[i + 1]
+
+    def peek(self):
+        return self.queue[self.front+1]
+    
+    def reverse_queue(self):
+        for i in range(self.size - 1, -1, -1):
+            reversed_queue=self.queue[i]
+        return reversed_queue

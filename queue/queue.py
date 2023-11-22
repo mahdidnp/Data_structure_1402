89cda21e5a748c5997863957db91138a32370979
zhrad81 <zhradashti8196@gmail.com>
2023-11-22 13:03:01 +0330

    :

--- queue/queue.py
+++ queue/queue.py
@@ -4,3 +4,18 @@ class Queue:
     def __init__(self,size) :
         self.size=size
         self.queue = [None]*size
+
+    def enqueue(self, item):
+        if self.rear==self.size:
+            self.is_full()
+        self.rear+=1
+        self.queue[self.rear]=item
+
+    def peek(self):
+        return self.queue[self.front+1]
+    
+    def is_full(self):
+        if self.rear==self.size:
+            return True
+        else:
+            return False

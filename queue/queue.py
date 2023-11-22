136dabef4163df6060b66cc79704db07bdca92ed
zhrad81 <zhradashti8196@gmail.com>
2023-11-22 13:12:12 +0330

    new methods added

--- queue/queue.py
+++ queue/queue.py
@@ -5,17 +5,3 @@ class Queue:
         self.size=size
         self.queue = [None]*size
 
-    def enqueue(self, item):
-        if self.rear==self.size:
-            self.is_full()
-        self.rear+=1
-        self.queue[self.rear]=item
-
-    def peek(self):
-        return self.queue[self.front+1]
-    
-    def is_full(self):
-        if self.rear==self.size:
-            return True
-        else:
-            return False

7407195fd5f22bcd94b4a6a3238f7ab380bc864b
mahdidnp <mahdidinpazhouh@gmail.com>
2023-11-23 14:15:40 +0330

    push added

--- stack/queue_with_stack.py
+++ stack/queue_with_stack.py
@@ -2,3 +2,6 @@ class queue_with_stack:
     def __init__(self):
         self.enqueue_stack=[]
         self.dequeue_stack=[]
+        
+    def push(self,item):
+        self.enqueue_stack.append(item)

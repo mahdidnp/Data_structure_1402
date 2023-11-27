ce07d0b59fd9d917dcebb62df26153062517e862
zhrad81 <zhradashti8196@gmail.com>
2023-11-27 14:19:02 +0330

    fix an eror

--- stack/queue_with_stack.py
+++ stack/queue_with_stack.py
@@ -8,10 +8,7 @@ class queue_with_stack:
     def pop(self):
         if len(self.dequeue_stack)==0:
             while len(self.enqueue_stack)==0:
-                last=self.enqueue_stack[-1]
-                del self.enqueue_stack[-1]
-                self.dequeue_stack.append(last)
-                #self.dequeue_stack.append(self.enqueue_stack[-1])
+                self.dequeue_stack.append(self.enqueue_stack[-1])
                 del self.enqueue_stack[-1]
 
         if not len(self.dequeue_stack)==0:

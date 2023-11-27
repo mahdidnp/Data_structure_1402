02fd969710f605e46504c5662aeb11bf899bb860
zhrad81 <zhradashti8196@gmail.com>
2023-11-27 14:10:58 +0330

    pop added

--- stack/queue_with_stack.py
+++ stack/queue_with_stack.py
@@ -5,3 +5,18 @@ class queue_with_stack:
         
     def push(self,item):
         self.enqueue_stack.append(item)
+    def pop(self):
+        if len(self.dequeue_stack)==0:
+            while len(self.enqueue_stack)==0:
+                last=self.enqueue_stack[-1]
+                del self.enqueue_stack[-1]
+                self.dequeue_stack.append(last)
+                #self.dequeue_stack.append(self.enqueue_stack[-1])
+                del self.enqueue_stack[-1]
+
+        if not len(self.dequeue_stack)==0:
+            item=self.dequeue_stack[-1]
+            del self.dequeue_stack[-1]
+            return item
+        else:
+            raise Exception("queue is empty")
